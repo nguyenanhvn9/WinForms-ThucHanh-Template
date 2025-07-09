4a9afeca03a11fcd4f6f750d5a2b187506011a03
BuiDucHieu-2180603383 <buiduchieu522003@gmail.com>
2025-07-09 16:12:38 +0700

    hieu

--- Tuan01/bai2,3/SinhVien.cs
+++
@@ -1,9 +0,0 @@
-﻿// File: SinhVien.cs
-using System;
-
-public class SinhVien
-{
-    public string MaSV { get; set; }
-    public string HoTen { get; set; }
-    public double DiemTB { get; set; }
-}
